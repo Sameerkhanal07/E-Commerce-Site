7556dbd877e8f302b7b8f736fd091e3fd1cf52c1
Sameer <sameerkhanal97@gmail.com>
2025-11-29 18:29:54 +0545

    Add cart functionality with models, views, and templates

---
+++ carts/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.cart, name='cart'),
+    path('add_cart/<int:product_id>/', views.add_cart, name='add_cart'),
+]
