1aa95b79de5711c7c16e983b84c358ab3417c009
Sameer <sameerkhanal97@gmail.com>
2025-11-29 21:46:29 +0545

    search_button

--- greatcart/settings.py
+++ greatcart/settings.py
@@ -66,6 +66,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
                 'category.context_processors.menu_links',  # Custom context processor
+                'carts.context_processors.counter',  # Custom context processor
             ],
         },
     },
